2654de5c88d6ac6a4280d93c738bc4566888ab7d
Gurpreet Singh <gurpreet.singh6@telus.com>
2022-11-18 01:30:06 +0530

    feat: first deployment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 
 function App() {
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [expire, setExpire] = useState(false);
 
-  useEffect(() => {
-    const countDownDate = new Date("Oct 28, 2022").getTime();
-    var x = setInterval(function () {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      setDays(days);
-      setHours(hours);
-      setMinutes(minutes);
-      setSeconds(seconds);
-      if (distance < 0) {
-        clearInterval(x);
-        setExpire(true);
-      }
-    }, 1000)
-  });
   return (
     <Fragment>
       <section className="cid-qIia8mgjVj mbr-fullscreen mbr-parallax-background" id="header2-0">
@@ -33,15 +9,8 @@ function App() {
           <div className="row justify-content-md-center">
             <div className="mbr-white col-md-10" style={{ color: '#000' }}>
               <h2 className="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
-                Once in a while,
-                <br />
-                right in the middle of an ordinary life,
-                <br />
-                love gives us a fairy tale
+                Yograj Passi Fireworks
               </h2>
-              <h3 className="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-2">
-                October 28, 2022
-              </h3>
             </div>
           </div>
         </div>
@@ -51,7 +20,7 @@ function App() {
           </a>
         </div>
       </section>
-      <section className="mbr-section content4 cid-qIieWoha50" id="content4-2">
+      {/* <section className="mbr-section content4 cid-qIieWoha50" id="content4-2">
         <div className="container">
           <div className="media-container-row">
             <div className="title col-12 col-md-8">
@@ -62,8 +31,8 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
-      <section className="mbr-section article content9 cid-qIifnNA7oY" id="content9-3">
+      </section> */}
+      {/* <section className="mbr-section article content9 cid-qIifnNA7oY" id="content9-3">
         <div className="container">
           <div className="inner-container" style={{ "width": "100%" }}>
             <hr className="line" style={{ "width": "25%" }} />
@@ -72,8 +41,8 @@ function App() {
             <hr className="line" style={{ "width": "25%" }} />
           </div>
         </div>
-      </section>
-      <section className="mbr-section content4 cid-qIifwA2R02" id="content4-4">
+      </section> */}
+      {/* <section className="mbr-section content4 cid-qIifwA2R02" id="content4-4">
         <div className="container">
           <div className="media-container-row">
             <div className="title col-12 col-md-8">
@@ -83,8 +52,8 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
-      <section className="mbr-section content5 cid-qIicQ8Vcwz mbr-parallax-background" id="content5-1">
+      </section> */}
+      {/* <section className="mbr-section content5 cid-qIicQ8Vcwz mbr-parallax-background" id="content5-1">
         <div className="container">
           <div className="media-container-row">
             <div className="title col-12 col-md-8">
@@ -94,10 +63,10 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       <br />
       <br />
-      <section className="features11 cid-qIix9MZdYF" id="features11-9">
+      {/* <section className="features11 cid-qIix9MZdYF" id="features11-9">
         <div className="container">
           <div className="col-md-12">
             <div className="media-container-row">
@@ -146,11 +115,10 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       <br />
       <br />
-      <section className="features11 cid-qIix9MZdYF" id="features11-9">
-
+      {/* <section className="features11 cid-qIix9MZdYF" id="features11-9">
         <div className="container">
           <div className="col-md-12">
             <div className="media-container-row">
@@ -208,8 +176,8 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
-      <section className="countdown1 cid-rdSMNc6MYv" id="countdown1-5">
+      </section> */}
+      {/* <section className="countdown1 cid-rdSMNc6MYv" id="countdown1-5">
         <div className="container ">
           <h2 className="mbr-section-title pb-1 align-center mbr-fonts-style display-2">
             It's not that far...
@@ -221,42 +189,14 @@ function App() {
           <div className="minutesCountdown" title="Minutes"></div>
           <div className="secondsCountdown" title="Seconds"></div>
           <div className="countdown pt-1 mt-2" data-due-date="2019/03/09">
-            <div className="row">
-              <div className="col-xs-3 col-sm-3 col-md-3">
-                <span className="number-wrap">
-                  <span className="number display-2">{expire ? '00' : days}</span>
-                  <span mbr-text="" className="period display-7">Days</span>
-                  <span className="dot">:</span>
-                </span>
-              </div>
-              <div className="col-xs-3 col-sm-3 col-md-3">
-                <span className="number-wrap">
-                  <span className="number display-2">{expire ? '00' : hours}</span>
-                  <span mbr-text="" className="period display-7">Hours</span>
-                  <span className="dot">:</span>
-                </span>
-              </div>
-              <div className="col-xs-3 col-sm-3 col-md-3">
-                <span className="number-wrap">
-                  <span className="number display-2">{expire ? '00' : minutes}</span>
-                  <span mbr-text="" className="period display-7">Minutes</span>
-                  <span className="dot">:</span>
-                </span>
-              </div>
-              <div className="col-xs-3 col-sm-3 col-md-3">
-                <span className="number-wrap">
-                  <span className="number display-2">{expire ? '00' : seconds}</span>
-                  <span mbr-text="" className="period display-7">Seconds</span>
-                </span>
-              </div>
-            </div>
+
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="map1 cid-qIiXmYb55q" id="map1-b">
         <div className="google-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.4820222508956!2d75.85346741544366!3d30.873173985900774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a82f5c895b741%3A0xea3ae52e5ef60e1c!2sAmantran%20Ville-%20Wedding%20venue%20and%20Banquet%20halls!5e0!3m2!1sen!2sin!4v1666210478481!5m2!1sen!2sin" width="600" height="450" style={{ "border": "0px" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3484.1988056470964!2d79.1576479154158!3d29.158813266816306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390a65be4e3c1955%3A0xa34a2e72008b5e05!2sYograj%20passi%20fireworks!5e0!3m2!1sen!2sin!4v1668715020866!5m2!1sen!2sin" width="600" height="450" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </section>
 
@@ -266,8 +206,6 @@ function App() {
             <div className="row row-copirayt">
               <p className="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-9">
                 © Copyright 2022 Gurpreet Singh - All Rights Reserved
-                <br />
-                Made with lots of <span className="fa fa-heart pulse2"></span> for Sunaina
               </p>
             </div>
           </div>
@@ -275,7 +213,7 @@ function App() {
       </section>
       <div id="scrollToTop" className="scrollToTop mbr-arrow-up"><a style={{ "textAlign": "center" }}><i></i></a></div>
       <input name="animation" type="hidden" />
-    </Fragment>
+    </Fragment >
   );
 }
 
