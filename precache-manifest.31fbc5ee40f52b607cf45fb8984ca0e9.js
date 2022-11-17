676df332af8ce1d2a77e991ea73aba07aac6ea67
Gurpreet Singh <gurpreet.singh6@telus.com>
2022-11-18 01:31:27 +0530

    Updates

---
+++ precache-manifest.31fbc5ee40f52b607cf45fb8984ca0e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac710a262bec2c02a53fa2127089c224",
+    "url": "/karan-passi/index.html"
+  },
+  {
+    "revision": "5027ae8a274eeb7165a6",
+    "url": "/karan-passi/static/js/2.79f8154f.chunk.js"
+  },
+  {
+    "revision": "fa674cd6ca6a4452646e",
+    "url": "/karan-passi/static/js/main.28cf9457.chunk.js"
+  },
+  {
+    "revision": "46dec2f9146eb0ec8c1b",
+    "url": "/karan-passi/static/js/runtime-main.d9145d28.js"
+  }
+]);
